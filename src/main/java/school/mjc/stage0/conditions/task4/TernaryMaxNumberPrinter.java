ab004e84045b0de59263cf35a4ea1e84d34f4fc6
Bilbo3340 <vladik.elce1@gmail.com>
2022-08-31 10:45:57 +0300

    upt

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,6 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        System.out.println(Math.max(Math.max(first, second), third));
     }
 }
